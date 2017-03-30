6288dfe1bb00b0823f34ab96afbf9ede60c8ccb2
fiorentinoing <stefano.fiore84@gmail.com>
2017-03-30 15:17:35 +0200

    mongod doesn't accept --logpath the way it worked on Mac OS X

--- mongo/mongo.sh
+++ mongo/mongo.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-# acknowledgement 
-# http://stackoverflow.com/users/3692084/ryannhg
-
 # Initialize a mongo data folder and logfile
 mkdir -p /data/db
-touch /var/log/mongodb.log
-chmod 777 /var/log/mongodb.log
 
-# Start mongodb with logging
-# --logpath    Without this mongod will output all log information to the standard output.
-# --logappend  Ensure mongod appends new entries to the end of the logfile. We create it first so that the below tail always finds something
-/entrypoint.sh mongod --logpath /var/log/mongodb.log --logappend &
+# Start mongodb 
+mongod &
 
-# Wait until mongo logs that it's ready (or timeout after 60s)
-COUNTER=0
-grep -q 'waiting for connections on port' /var/log/mongodb.log
-while [[ $? -ne 0 && $COUNTER -lt 60 ]] ; do
-    sleep 2
-    let COUNTER+=2
-    echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
-    grep -q 'waiting for connections on port' /var/log/mongodb.log
-done
+# Wait mongo for 10 sec
+sleep 10
 
 # Restore from dump
 mongorestore --drop --db mp /home/dump/mp
